ee779548eb5ba78e18b843360a84bac53665c490
Vchandankumarr <chandankumarav.18@gmail.com>
2023-07-29 21:20:40 +0530

    use reff

--- RCT101/day8/usereff/src/components/Form.jsx
+++ RCT101/day8/usereff/src/components/Form.jsx
@@ -14,21 +14,35 @@
 
 
     const handlechange=(e)=>{
+        console.log(e.target.value)
+        // for check box e.target.checked
+        // console.log(e.target.checked)
+
+        const val=e.target.type==="checkbox"?e.target.checked:e.target.value
     setFormstate({...formstate,
-        [e.target.name]:e.target.value})
+        [e.target.name]:val})
     }
 
+
+    const handlesubmit=(e)=>{
+        e.preventDefault()
+        console.log(formstate)
+        console.log("inside form")
+
+    }
+
+    const {username,country,isMarried}=formstate
     return (
         <div>
             <h1>FORMS</h1>
-            <form >
+            <form onSubmit={handlesubmit}  >
                 <label>
                     Username:-{" "}
-                    <input name="username" type="text" value={formstate.username} onChange={handlechange} />
+                    <input name="username" type="text" value={username} onChange={handlechange} required />
                 </label>
         <br /><br />
                 <label >Country:-{" "}
-                    <select name="country"  value={formstate.country} onChange={handlechange} >
+                    <select name="country"  value={country} onChange={handlechange} required>
                         <option value="India">India</option>
                         <option value="Japan">Japan</option>
                     </select>
@@ -36,10 +50,10 @@
                         <br /><br />
                 <label>
                     IsMarried
-                    <input name="isMarried" type="checkbox" value={formstate.isMarried} onChange={handlechange}  />
+                    <input name="isMarried" type="checkbox" value={isMarried} onChange={handlechange}  required/>
                 </label>
                 <br /><br />
-                
+                   
                 <input type="submit" />
             </form>
         </div>
